23c081ddf4a9aed94da77eb9b13c0f93ea11ec14
Leonardo Azocar <leonardoazocar_v16@hotmail.com>
2016-06-03 00:51:41 -0430

    Add files via upload

---
+++ Inserts Emp_Ben.sql
@@ -0,0 +1,5 @@
+﻿insert into emp_ben (eb_fecha,eb_fk_empleado,eb_fk_beneficio) values ('12/12/2011','24407023',4);
+insert into emp_ben (eb_fecha,eb_fk_empleado,eb_fk_beneficio) values ('12/12/2010','20914448',1);
+insert into emp_ben (eb_fecha,eb_fk_empleado,eb_fk_beneficio) values ('12/12/2005','21618128',5);
+insert into emp_ben (eb_fecha,eb_fk_empleado,eb_fk_beneficio) values ('12/12/2003','123456789',3);
+insert into emp_ben (eb_fecha,eb_fk_empleado,eb_fk_beneficio) values ('12/11/2001','987654321',2);
---
+++ Inserts Horario.sql
@@ -0,0 +1,7 @@
+﻿insert into horario (hor_dia,hor_horainicio,hot_horafin) values ('Lunes','12/12/1234 07:00:00','12/12/1234 17:00:00');
+insert into horario (hor_dia,hor_horainicio,hot_horafin) values ('Martes','12/12/1234 07:00:00','12/12/1234 17:00:00');
+insert into horario (hor_dia,hor_horainicio,hot_horafin) values ('Miercoles','12/12/1234 07:00:00','12/12/1234 17:00:00');
+insert into horario (hor_dia,hor_horainicio,hot_horafin) values ('Jueves','12/12/1234 07:00:00','12/12/1234 17:00:00');
+insert into horario (hor_dia,hor_horainicio,hot_horafin) values ('Viernes','12/12/1234 07:00:00','12/12/1234 17:00:00');
+insert into horario (hor_dia,hor_horainicio,hot_horafin) values ('Sabado','12/12/1234 09:00:00','12/12/1234 15:00:00');
+insert into horario (hor_dia,hor_horainicio,hot_horafin) values ('Domingo','12/12/1234 09:00:00','12/12/1234 15:00:00');
