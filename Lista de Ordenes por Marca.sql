14dbe159308dd15b35998f13791c0b2384f83d5f
Leonardo Azocar <leonardoazocar_v16@hotmail.com>
2016-06-03 00:58:08 -0430

    Add files via upload

---
+++ Lista de Ordenes por Marca.sql
@@ -0,0 +1,4 @@
+﻿select count(des_codigo) as nroOrdenes, mar_nombre
+from marca, despacho, producto,ejemplar
+where eje_fk_despacho=des_codigo and eje_fk_producto=pro_codigo and pro_fk_marca=mar_codigo
+group by mar_nombre
---
+++ Listado de marcas por productos.sql
@@ -0,0 +1,5 @@
+﻿select mar_nombre, pro_razonsocial
+from marca, producto, ejemplar,proveedor,despacho
+where pro_rif=des_fk_proveedor and des_codigo=eje_fk_despacho and eje_fk_producto=pro_codigo and pro_fk_marca=mar_codigo
+group by mar_nombre, pro_razonsocial
+order by pro_razonsocial, mar_nombre
